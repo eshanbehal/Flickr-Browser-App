58ad37f037e33b23774db9bf9c99214d96b336f5
eshan786 <eshan786behal@gmail.com>
2019-06-19 13:50:38 +0530

    added new classes nd written code.

---
+++ app/src/main/java/eshanbehal/flickrbrowser/GetFlickrJsonData.java
@@ -0,0 +1,54 @@
+package eshanbehal.flickrbrowser;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.List;
+
+class GetFlickrJsonData implements GetRawData.OnDownloadComplete {
+    private static final String TAG = "GetFlickrJsonData";
+
+    private List<Photo> mPhotoList = null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    private final OnDataAvailable mCallBack;
+
+    interface OnDataAvailable{
+        void onDataAvailable(List<Photo> data , DownloadStatus status);
+    }
+
+    public GetFlickrJsonData(String baseURL, String language, boolean matchAll, OnDataAvailable callBack) {
+        Log.d(TAG, "GetFlickrJsonData: called");
+        mBaseURL = baseURL;
+        mLanguage = language;
+        mMatchAll = matchAll;
+        mCallBack = callBack;
+    }
+
+    void executeOnSameThread(String searchCriteria){
+
+        Log.d(TAG, "executeOnSameThread: starts");
+        String destinationUri = createUri(searchCriteria , mLanguage , mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: ends");
+    }
+
+    private String createUri(String searchCriteria , String lang , boolean matchAll){
+        Log.d(TAG, "createUri: starts");
+        return Uri.parse(mBaseURL).buildUpon()
+                .appendQueryParameter("tags" , searchCriteria)
+                .appendQueryParameter("tagmode" , matchAll? "ALL":"ANY")
+                .appendQueryParameter("lang" , lang)
+                .appendQueryParameter("format" , "json")
+                .appendQueryParameter("nojsoncallback" , "1")
+                .build().toString();
+        
+
+
+    }
+
+}
