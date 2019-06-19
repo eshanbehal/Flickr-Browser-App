3c195528bb25d0153c75340e9d5b3e068aa222d3
eshan786 <eshan786behal@gmail.com>
2019-06-19 17:53:29 +0530

    completed code.

--- app/src/main/java/eshanbehal/flickrbrowser/GetFlickrJsonData.java
+++ app/src/main/java/eshanbehal/flickrbrowser/GetFlickrJsonData.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -19,62 +20,101 @@ class GetFlickrJsonData implements GetRawData.OnDownloadComplete {
 
     private final OnDataAvailable mCallBack;
 
-    interface OnDataAvailable{
-        void onDataAvailable(List<Photo> data , DownloadStatus status);
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
     }
 
-    public GetFlickrJsonData(String baseURL, String language, boolean matchAll, OnDataAvailable callBack) {
-        Log.d(TAG, "GetFlickrJsonData: called");
+    public GetFlickrJsonData(OnDataAvailable callBack, String baseURL, String language, boolean matchAll) {
+        Log.d(TAG, "GetFlickrJsonData called");
         mBaseURL = baseURL;
+        mCallBack = callBack;
         mLanguage = language;
         mMatchAll = matchAll;
-        mCallBack = callBack;
     }
 
-    void executeOnSameThread(String searchCriteria){
-
-        Log.d(TAG, "executeOnSameThread: starts");
-        String destinationUri = createUri(searchCriteria , mLanguage , mMatchAll);
+    void executeOnSameThread(String searchCriteria) {
+        Log.d(TAG, "executeOnSameThread starts");
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
 
         GetRawData getRawData = new GetRawData(this);
         getRawData.execute(destinationUri);
-        Log.d(TAG, "executeOnSameThread: ends");
+        Log.d(TAG, "executeOnSameThread ends");
     }
 
-    private String createUri(String searchCriteria , String lang , boolean matchAll){
-        Log.d(TAG, "createUri: starts");
+    private String createUri(String searchCriteria, String lang, boolean matchAll) {
+        Log.d(TAG, "createUri starts");
+
         return Uri.parse(mBaseURL).buildUpon()
-                .appendQueryParameter("tags" , searchCriteria)
-                .appendQueryParameter("tagmode" , matchAll? "ALL":"ANY")
-                .appendQueryParameter("lang" , lang)
-                .appendQueryParameter("format" , "json")
-                .appendQueryParameter("nojsoncallback" , "1")
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
                 .build().toString();
     }
 
     @Override
     public void onDownloadComplete(String data, DownloadStatus status) {
-        Log.d(TAG, "onDownloadComplete: starts. Status = " + status);
+        Log.d(TAG, "onDownloadComplete starts. Status = " + status);
 
-        if (status == DownloadStatus.OK){
+        if(status == DownloadStatus.OK) {
             mPhotoList = new ArrayList<>();
-        }
-        try {
-            JSONObject jsonData = new JSONObject(data);
-            JSONArray itemsArray = jsonData.getJSONArray("Items");
-            for (int i = 0 ; i<itemsArray.length() ; i++){
-                JSONObject jsonPhoto = itemsArray.getJSONObject(i);
-                String title = jsonPhoto.getString("title");
-                String author = jsonPhoto.getString("author");
-                String authorId = jsonPhoto.getString("author_id");
-                String tags = jsonPhoto.getString("tags");
 
-                JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
-                String PhotoUrl = jsonMedia.getString("m");
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
 
-                String link = PhotoUrl.replaceFirst("_m." , "_b.");
+                for(int i=0; i<itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authorId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
 
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+
+                    Photo photoObject = new Photo(title, author, authorId, link, tags, photoUrl);
+                    mPhotoList.add(photoObject);
+
+                    Log.d(TAG, "onDownloadComplete " + photoObject.toString());
+                }
+            } catch(JSONException jsone) {
+                jsone.printStackTrace();
+                Log.e(TAG, "onDownloadComplete: Error processing Json data " + jsone.getMessage());
+                status = DownloadStatus.FAILED_OR_EMPTY;
             }
         }
+
+        if(mCallBack != null) {
+            // now inform the caller that processing is done - possibly returning null if there
+            // was an error
+            mCallBack.onDataAvailable(mPhotoList, status);
+        }
+
+        Log.d(TAG, "onDownloadComplete ends");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/eshanbehal/flickrbrowser/MainActivity.java
+++ app/src/main/java/eshanbehal/flickrbrowser/MainActivity.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements GetRawData.OnDownloadComplete {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
     private static final String TAG = "MainActivity";
 
     @Override
@@ -15,13 +17,22 @@ public class MainActivity extends AppCompatActivity implements GetRawData.OnDown
         Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GetRawData getRawData = new GetRawData(this);
-        getRawData.execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
+//        GetRawData getRawData = new GetRawData(this);
+//        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
 
         Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume starts");
+        super.onResume();
+        GetFlickrJsonData getFlickrJsonData = new GetFlickrJsonData(this,"https://api.flickr.com/services/feeds/photos_public.gne", "en-us", true);
+        getFlickrJsonData.executeOnSameThread("android, nougat");
+        Log.d(TAG, "onResume ends");
     }
 
     @Override
@@ -50,12 +61,26 @@ public class MainActivity extends AppCompatActivity implements GetRawData.OnDown
 
     @Override
 
-    public void onDownloadComplete(String data, DownloadStatus status) {
+    public void onDataAvailable(List<Photo> data, DownloadStatus status) {
         if(status == DownloadStatus.OK) {
-            Log.d(TAG, "onDownloadComplete: data is " + data);
+            Log.d(TAG, "onDataAvailable: data is " + data);
         } else {
             // download or processing failed
-            Log.e(TAG, "onDownloadComplete failed with status " + status);
+            Log.e(TAG, "onDataAvailable failed with status " + status);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
