9ad128501c5eb106a426780e42580cdfcc400da3
eshan786 <eshan786behal@gmail.com>
2019-06-20 12:30:42 +0530

    made last changes to code.

--- app/src/main/java/eshanbehal/flickrbrowser/GetRawData.java
+++ app/src/main/java/eshanbehal/flickrbrowser/GetRawData.java
@@ -30,7 +30,10 @@ class GetRawData extends AsyncTask<String, Void, String> {
 
     void runInSameThread(String s){
         Log.d(TAG, "runInSameThread: starts");
-        onPostExecute(doInBackground(s));
+//        onPostExecute(doInBackground(s));
+        if (mCallback != null){
+            mCallback.onDownloadComplete(doInBackground(s) , mDownloadStatus);
+        }
         Log.d(TAG, "runInSameThread: ends");
     }
 
