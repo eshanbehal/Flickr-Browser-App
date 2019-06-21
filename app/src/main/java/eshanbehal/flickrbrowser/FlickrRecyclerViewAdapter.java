7a9c99d281667b9408d01d64781e4d59ecd85abd
eshan786 <eshan786behal@gmail.com>
2019-06-21 12:35:51 +0530

    modified code.

---
+++ app/src/main/java/eshanbehal/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,39 @@
+package eshanbehal.flickrbrowser;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder>{
+
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotoList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter(List<Photo> photoList, Context context) {
+        mPhotoList = photoList;
+        mContext = context;
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder{
+
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+
+
+}
