e8cd404d33cefdd6bcb659320a0d63a8221daf46
eshan786 <eshan786behal@gmail.com>
2019-06-24 16:48:23 +0530

    added gesture detectors.

---
+++ app/src/main/java/eshanbehal/flickrbrowser/RecyclerItemClickListener.java
@@ -0,0 +1,36 @@
+package eshanbehal.flickrbrowser;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener {
+
+    private static final String TAG = "RecyclerItemClickListen";
+
+    interface OnRecyclerClickListener{
+
+        void onItemClick(View view , int position);
+        void onItemLongClick(View view , int position);
+
+    }
+
+    private final OnRecyclerClickListener mListener;
+    private final GestureDetectorCompat mGestureDetector;
+
+    public RecyclerItemClickListener(Context context , final RecyclerView recyclerView , OnRecyclerClickListener listener) {
+        mListener = listener;
+        mGestureDetector = null;
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        Log.d(TAG, "onInterceptTouchEvent: starts");
+        return super.onInterceptTouchEvent(rv, e);
+    }
+}
