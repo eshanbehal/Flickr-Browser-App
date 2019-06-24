77c4c5cddf56670b5415f72515c3a2b7fc1ef47b
eshan786 <eshan786behal@gmail.com>
2019-06-24 17:15:26 +0530

    touch events added.

--- app/src/main/java/eshanbehal/flickrbrowser/RecyclerItemClickListener.java
+++ app/src/main/java/eshanbehal/flickrbrowser/RecyclerItemClickListener.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -24,13 +25,31 @@ class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener {
 
     public RecyclerItemClickListener(Context context , final RecyclerView recyclerView , OnRecyclerClickListener listener) {
         mListener = listener;
-        mGestureDetector = null;
+        mGestureDetector = new GestureDetectorCompat(context , new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return super.onSingleTapUp(e);
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                super.onLongPress(e);
+            }
+        });
 
     }
 
     @Override
     public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
         Log.d(TAG, "onInterceptTouchEvent: starts");
-        return super.onInterceptTouchEvent(rv, e);
+
+        if (mGestureDetector != null){
+            boolean result = mGestureDetector.onTouchEvent(e);
+            Log.d(TAG, "onInterceptTouchEvent(): returned" + result);
+            return result;
+        }else {
+            Log.d(TAG, "onInterceptTouchEvent(): returned: flase");
+            return false;
+        }
     }
 }
