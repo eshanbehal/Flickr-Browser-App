c9ca0042bcf1e90eb30212bf3f2fe4ec2647ffef
eshan786 <eshan786behal@gmail.com>
2019-06-29 17:10:28 +0530

    added search bar for the app

--- app/src/main/java/eshanbehal/flickrbrowser/PhotoDetailActivity.java
+++ app/src/main/java/eshanbehal/flickrbrowser/PhotoDetailActivity.java
@@ -27,6 +27,7 @@ public class PhotoDetailActivity extends BaseActivity {
             //photoTitle.setText("Title: " + photo.getTitle());
 
             TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+            photoTags.setText(resources.getString(R.string.photo_tags_text , photo.getTags()));
             //photoTags.setText("Tags: " + photo.getTags());
 
             TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
