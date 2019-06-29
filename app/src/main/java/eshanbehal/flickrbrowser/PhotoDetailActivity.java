16b7f09015a96a893a9266a9815f56debf612974
eshan786 <eshan786behal@gmail.com>
2019-06-29 16:46:12 +0530

    worked on string resources.

--- app/src/main/java/eshanbehal/flickrbrowser/PhotoDetailActivity.java
+++ app/src/main/java/eshanbehal/flickrbrowser/PhotoDetailActivity.java
@@ -1,6 +1,7 @@
 package eshanbehal.flickrbrowser;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,10 +21,13 @@ public class PhotoDetailActivity extends BaseActivity {
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
         if(photo != null) {
             TextView photoTitle = (TextView) findViewById(R.id.photo_title);
-            photoTitle.setText("Title: " + photo.getTitle());
+            Resources resources = getResources();
+            String text = resources.getString(R.string.photo_title_text , photo.getTitle());
+            photoTitle.setText(text);
+            //photoTitle.setText("Title: " + photo.getTitle());
 
             TextView photoTags = (TextView) findViewById(R.id.photo_tags);
-            photoTags.setText("Tags: " + photo.getTags());
+            //photoTags.setText("Tags: " + photo.getTags());
 
             TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
             photoAuthor.setText(photo.getAuthor());
