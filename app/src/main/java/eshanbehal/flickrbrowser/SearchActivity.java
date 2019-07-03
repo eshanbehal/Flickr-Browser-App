1f4ef3cc4a4f39592c51cfc893a2c214fc3308d6
eshan786 <eshan786behal@gmail.com>
2019-07-03 12:21:37 +0530

    implemented shared prefrences in search view.

--- app/src/main/java/eshanbehal/flickrbrowser/SearchActivity.java
+++ app/src/main/java/eshanbehal/flickrbrowser/SearchActivity.java
@@ -3,7 +3,9 @@ package eshanbehal.flickrbrowser;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.SearchView;
@@ -42,6 +44,9 @@ public class SearchActivity extends BaseActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Log.d(TAG, "onQueryTextSubmit: called");
+
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                sharedPreferences.edit().putString(FLICKR_QUERY , query).apply();
                 mSearchView.clearFocus();
                 finish();
                 return true;
@@ -49,6 +54,15 @@ public class SearchActivity extends BaseActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+
+                finish();
                 return false;
             }
         });
